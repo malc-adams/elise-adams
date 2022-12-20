e6e6a4581f0e8368f3dbf78c079a935da9ca39e7
malc-adams <47540589+malc-adams@users.noreply.github.com>
2022-12-20 14:53:19 +0000

    Clean up

--- src/components/SoundItem.js
+++ src/components/SoundItem.js
@@ -6,7 +6,12 @@ export default function SoundItem() {
     return (
         <>
             <div className="sound-item">
-                <img src="https://pickchurz.imgix.net/elise/cat.png?fm=png&w=100" alt="cat" />
+                <img
+                    srcSet="https://pickchurz.imgix.net/elise/cat.png?auto=format&fm=png&w=100&dpr=2 2x, https://pickchurz.imgix.net/elise/cat.png?auto=format&fm=png&w=100 1x"
+                    src="https://pickchurz.imgix.net/elise/cat.png?auto=format&fm=png&w=100&dpr=2"
+                    alt="cat"
+                    loading="eager"
+                />
                 <Button className="big-button" size="lg">cat</Button>
             </div>
         </>
