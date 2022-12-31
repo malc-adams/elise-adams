e35f00bc8be8eeab03eefaf0aa700abc3b8c7775
malc-adams <47540589+malc-adams@users.noreply.github.com>
2022-12-31 15:12:37 +0000

    World

--- src/components/Body.js
+++ src/components/Body.js
@@ -11,6 +11,10 @@ export default function Body() {
               <SoundItem animal="pig" sound="174614__yottasounds__pig004" />
               <SoundItem animal="chicken" sound="316920__rudmer-rotteveel__chicken-single-alarm-call" />
           </div>
+          <h2>Explore the World</h2>
+          <div className="item-container">
+              <a href="https://www.google.com/maps/@51.3426135,-2.9832091,378m/data=!3m1!1e3" target="_blank">View the world</a>
+          </div>
         </div>
     )
 }
